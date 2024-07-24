73877598b08d44ded8786cece544f5bc2d9a3ccb
tongxiao <tongxiao2002@mail.ustc.edu.cn>
2024-07-24 14:13:05 +0800

    upgrade, create wrapper class

--- llm_api_access/__init__.py
+++ llm_api_access/__init__.py
@@ -1,8 +1,3 @@
-import os
-import logging
-from typing import Callable
-from .utils import get_logger, llm_inputs_wrapper, omit_existing_data_wrapper
-from .llm_runner import LLMRunner
 from .arguments import (
     parse_args,
     EntireArguments,
@@ -11,55 +6,14 @@ from .arguments import (
     DataArguments,
     RunningArguments,
 )
+from .wrapper import LLMRunnerWrapper
 
 __all__ = [
     'parse_args',
-    'run_llm_api',
-    'llm_inputs_wrapper',
-    'omit_existing_data_wrapper',
     'EntireArguments',
     'LLMArguments',
+    'DataArguments',
     'GenerationArguments',
     'RunningArguments',
-    'LLMRunner',
+    'LLMRunnerWrapper',
 ]
-
-
-def run_llm_api(
-    arguments: EntireArguments,
-    prompt_template: str,
-    llm_input_process_func: Callable,
-    llm_output_postprocess_func: Callable,
-    data_load_func: Callable,
-):
-    output_filepath = arguments.output_filepath
-
-    if arguments.generate_log_file:
-        logger = get_logger(output_dir=os.path.dirname(output_filepath))
-    else:
-        logger = logging.getLogger(__file__)
-
-    data_args = DataArguments.from_args(arguments)
-    dataset = data_load_func(data_args)
-
-    if len(dataset) == 0:
-        logger.info("There is no data need to be run. Experiment finished.")
-        return
-
-    logger.info(
-        f"Model: {arguments.llm}, # Data Items: {len(dataset)}"
-    )
-
-    # load & run llm
-    runner = LLMRunner(
-        arguments=arguments,
-        prompt_template=prompt_template,
-        producer_process_func=llm_input_process_func,
-        consumer_postprocess_func=llm_output_postprocess_func,
-        logger=logger,
-    )
-    runner.run(
-        data_items=dataset,
-        num_threads=arguments.num_threads,
-        output_filename=output_filepath,
-    )
---
+++ llm_api_access/wrapper.py
@@ -0,0 +1,65 @@
+import os
+import logging
+from .utils import (
+    get_logger,
+    llm_inputs_wrapper,
+    omit_existing_data_wrapper,
+)
+from .arguments import (
+    DataArguments,
+    EntireArguments,
+)
+from .llm_runner import LLMRunner
+
+
+class LLMRunnerWrapper:
+    def __init__(self, arguments: EntireArguments):
+        self.arguments = arguments
+
+        if self.arguments.generate_log_file:
+            self.logger = get_logger(output_dir=os.path.dirname(self.arguments.output_filepath))
+        else:
+            self.logger = logging.getLogger(__file__)
+
+    @omit_existing_data_wrapper
+    def load_data(self, data_args: DataArguments):
+        raise NotImplementedError
+
+    @llm_inputs_wrapper
+    def prepare_llm_inputs(self, inputs: dict, prompt_template: str):
+        raise NotImplementedError
+
+    def postprocess_llm_outputs(self, inputs: dict, response: str, prompt: str):
+        raise NotImplementedError
+
+    def run_llm_api(
+        self,
+        arguments: EntireArguments,
+        prompt_template: str,
+    ):
+        output_filepath = self.arguments.output_filepath
+
+        data_args = DataArguments.from_args(self.arguments)
+        dataset = self.load_data(data_args)
+
+        if len(dataset) == 0:
+            self.logger.info("There is no data need to be run. Experiment finished.")
+            return
+
+        self.logger.info(
+            f"Model: {arguments.llm}, # Data Items: {len(dataset)}"
+        )
+
+        # load & run llm
+        runner = LLMRunner(
+            arguments=arguments,
+            prompt_template=prompt_template,
+            producer_process_func=self.prepare_llm_inputs,
+            consumer_postprocess_func=self.postprocess_llm_outputs,
+            logger=self.logger,
+        )
+        runner.run(
+            data_items=dataset,
+            num_threads=arguments.num_threads,
+            output_filename=output_filepath,
+        )
